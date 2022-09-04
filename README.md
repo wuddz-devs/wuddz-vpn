27b7530d58a2f2c08a230b7e3431f43d07a3f89d
wuddz-devs <wuddz_devs@protonmail.com>
2022-09-04 00:12:55 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# wuddz-vpn
-Connect To NordVPN Or Alternate Provider Servers With A Valid Account, Using Openvpn &amp; Ovpn Configs
+# wuddz-vpn 
+- Connect To NordVPN Or Alternate Provider Servers, With A Valid Account, Using Openvpn & Ovpn Configs
+
+
+## Description
+- Wuddz-Vpn Is An Awesome, Efficient And Handy Vpn Connection Program, Using Python & Installed Openvpn Client.
+
+- Connect To A Random NordVPN Server In Location Of Choice, With Chosen Protocol (UDP or TCP), Using Provided '.ovpn' Config Files.
+ 
+- Connect To A Random Alternate VPN Provider Server, Using Downloaded '.ovpn' Config Files Of VPN Provider.
+
+
+## Requirements
+- Python
+- Openvpn Client
+- Valid NordVPN Or Alternate Provider Account
+
+
+## Setup
+- Install python  [Link: https://www.python.org/downloads/]
+
+- Install openvpn  [Link: https://openvpn.net/community-downloads]
+
+- Download Alternate VPN Provider's '.ovpn' Config Files For Servers Your Account Is Capable Of Connecting To, Copy & Paste Them In "alt_configs" Folder.
+
+- Replace "username" & "password" In "vpncreds.txt" File With Username & Password Of Valid VPN Account Respectively.
+
+
+## Benefits & Usage:
+- Open "cmd" | "powershell" | "terminal" depending on your OS platform, navigate to repository root folder with wuddz_vpn.py file
+
+- Type Or Paste "python wuddz_vpn.py" To Launch Wuddz-Vpn
+
+- If Installed Openvpn Client On Windows Is Not In "C:\Program Files\OpenVPN\bin" Simply Choose "p" At Menu To Specify "openvpn.exe" File Path
+
+- When VPN Connection Is Initialized The Edited ".ovpn" Config File Of Server Being Connected To Is Copied To The Repository Root Folder & Removed When VPN Is Disconnected.
+
+- Using This Handy Tool Negates The Need To Download Client GUI Programs Which Can Be Laden With Ads & Are Sometimes Buggy
+
+- Just Choose Option You Want And In The Blink Of An Eye You'll Be Both Connected & Indeed Protected Using Your Favorite VPN Provider
+
+- It Truly Is That Simple & Convenient Yet Hassle Free, Just Follow The Onscreen Instructions Provided And You'll Be Good To Go.
+
+
+## Contact Info:
+- Email:     wuddz_devs@protonmail.com
+- Github:    https://github.com/wuddz-devs
+- Telegram:  https://t.me/wuddz_devs
+- Videos:    https://mega.nz/folder/IWVAXTqS#FoZAje2NukIcIrEXXKTo0w
+- Reddit:    https://reddit.com/user/wuddz-devs
+- Youtube:   wuddz-devs
+
+
+### Buy Me A Coffee
+- Ethereum: 0x1F1C47dD653Af628D394eac7bAA9Ccf774fd784f
+
+
+#### Enjoy my awesome creativity!!
+#### Peace & Love Always!!
---
+++ wuddz_vpn.py
@@ -0,0 +1,149 @@
+"""
+░░░░░░░░░░░██╗░░░░░░░██╗██╗░░░██╗██████╗░██████╗░███████╗░░░░░░██╗░░░██╗██████╗░███╗░░██╗░░░░░░░░░░░
+░░░░░░░░░░░██║░░██╗░░██║██║░░░██║██╔══██╗██╔══██╗╚════██║░░░░░░██║░░░██║██╔══██╗████╗░██║░░░░░░░░░░░
+░░░░░░░░░░░╚██╗████╗██╔╝██║░░░██║██║░░██║██║░░██║░░███╔═╝█████╗╚██╗░██╔╝██████╔╝██╔██╗██║░░░░░░░░░░░
+░░░░░░░░░░░░████╔═████║░██║░░░██║██║░░██║██║░░██║██╔══╝░░╚════╝░╚████╔╝░██╔═══╝░██║╚████║░░░░░░░░░░░
+░░░░░░░░░░░░╚██╔╝░╚██╔╝░╚██████╔╝██████╔╝██████╔╝███████╗░░░░░░░░╚██╔╝░░██║░░░░░██║░╚███║░░░░░░░░░░░
+░░░░░░░░░░░░░╚═╝░░░╚═╝░░░╚═════╝░╚═════╝░╚═════╝░╚══════╝░░░░░░░░░╚═╝░░░╚═╝░░░░░╚═╝░░╚══╝░░░░░░░░░░░
+
+ [*]Descr:     CONNECT TO RANDOM NORDVPN SERVER OR AN ALTERNATE PROVIDER SERVER WITH A VALID ACCOUNT
+ [*]Coder:     Wuddz_Devs                                                                           
+ [*]Email:     wuddz_devs@protonmail.com                                                            
+ [*]Github:    https://github.com/wuddz-devs                                                        
+ [*]Telegram:  https://t.me/wuddz_devs                                                              
+ [*]Videos:    https://mega.nz/folder/IWVAXTqS#FoZAje2NukIcIrEXXKTo0w                               
+ [*]Reddit:    https://reddit.com/users/wuddz-devs                                                  
+ [*]Youtube:   wuddz-devs                                                                           
+
+ [*]Menu:
+    1    =>    Connect To NordVPN Servers                          
+    2    =>    Connect To Alternate Servers                        
+    p    =>    Specify Openvpn Executable File Path Or Default Used
+    e    =>    Exit Program                                        
+"""
+
+import sys, re, random
+from subprocess import Popen, PIPE
+from pathlib import Path
+from os import name, path, remove, system
+system('')
+
+
+def clear_screen():
+    if name=='nt':system('cls')
+    else:system('clear')
+
+def get_menu():
+    a=input('\n\033[1;32;40m...Hit Enter|Return Key To Continue....\033[0m\n') or None
+    if a:return
+
+def get_config(ptc,vpnc=None):
+    clear_screen()
+    try:
+        vpnl=Path.joinpath(Path.cwd(),ptc)
+        if vpnc!=None:
+            if '-' in str(vpnc):cnf=list(Path(vpnl).rglob(vpnc+'*.ovpn'))
+            else:cnf=[str(v) for v in list(Path(vpnl).rglob(vpnc+'*.ovpn')) if not '-' in str(Path(v).stem)]
+        else:cnf=[str(x) for x in list(Path(vpnl).rglob('*.ovpn'))]
+        ove=random.choice(cnf)
+        o,p=path.split(ove)
+        ovp=Path.joinpath(Path.cwd(),p)
+        with open(ove, 'r', encoding='utf-8') as ovr:
+            with open(ovp, 'w', encoding='utf-8') as ovw:
+                for line in ovr:
+                    ovw.write(str(line).replace('cipher AES-256-CBC', 'data-ciphers-fallback AES-256-CBC'))
+        return ovp,ove
+    except:pass
+
+def nord_location(pc,opvpn):
+    """ [*]NordVPN Country Codes:
+    al = Albania      cy = Cyprus           in = India         nl = Netherlands       es = Spain        
+    ar = Argentina    cz = CzechRepublic    id = Indonesia     nz = NewZealand        se = Sweden       
+    au = Australia    dk = Denmark          ie = Ireland       mk = NorthMacedonia    ch = Switzerland  
+    at = Austria      ee = Estonia          il = Israel        pl = Poland            tw = Taiwan       
+    be = Belgium      fi = Finland          it = Italy         pt = Portugal          th = Thailand     
+    br = Brazil       fr = France           jp = Japan         ro = Romania           tr = Turkey       
+    ba = Bosnia       de = Germany          lv = Latvia        rs = Serbia            uk = UnitedKingdom
+    bg = Bulgaria     ge = Georgia          lt = Lithuania     sg = Singapore         ua = Ukraine      
+    ca = Canada       gr = Greece           lu = Luxembourg    sk = Slovakia          us = USA          
+    cl = Chile        hk = HongKong         md = Moldova       si = Slovenia          vn = Vietnam      
+    cr = CostaRica    hu = Hungary          my = Malaysia      za = SouthAfrica                         
+    hr = Croatia      is = Iceland          mx = Mexico        kr = SouthKorea                          
+
+    DoubleVPN Country Codes:
+    ca-us = Canada-USA                      nl-ch = Netherlands-Switzerland
+    ch-nl = Switzerland-Netherlands         uk-fr = UK-France              
+    ch-se = Switzerland-Sweden              uk-nl = UK-Netherlands         
+    fr-uk = France-UK                       se-ch = Sweden-Switzerland     
+    hk-tw = HongKong-Taiwan                 se-nl = Sweden-Netherlands     
+    nl-se = Netherlands-Sweden              tw-hk = Taiwan-HongKong        
+    nl-uk = Netherlands-UK                  us-ca = USA-Canada             
+
+    Country Code    =>    [e.g us = Connects To Random Nord USA Vpn Server]
+    Ctrl+C          =>    Disconnects VPN If Connected                     
+    b               =>    Back To Previous Screen                          
+"""
+    ptc='nord_udp'
+    if pc=='t':ptc='nord_tcp'
+    while True:
+        try:
+            clear_screen()
+            vpna=input('\033[1;32;40m'+nord_location.__doc__+'\033[0m\nInput Country Code or b=> ') or None
+            if vpna=='b':break
+            elif vpna.lower()+' =' in str(nord_location.__doc__):
+                ovp,ove=get_config(ptc,vpnc=vpna.lower())
+                vpn_connect(ovp,opvpn)
+        except:pass
+
+def vpn_connect(ovp,opvpn):
+    try:
+        vpna=Path.joinpath(Path.cwd(),'vpncreds.txt')
+        with Popen([opvpn, '--config', ovp, '--auth-user-pass', vpna, '--connect-timeout', '10', '--connect-retry-max', '2', '--remap-usr1', 'SIGTERM'], shell=False, stdout=PIPE, bufsize=1, universal_newlines=True) as vco:
+            for l in vco.stdout:
+                if str(l).count('AUTH_FAILED')!=0:print('\n\033[1;34;40mAccount Not Valid\033[0m')
+                elif str(l).count('Initialization Sequence Completed')!=0:
+                    print('\n\033[1;34;40mConnected To '+str(path.split(ovp)[1])+'\033[0m')
+    except:pass
+    print('\n\033[1;31;40m*VPN Not Connected*\033[0m')
+    remove(ovp)
+    get_menu()
+
+def nord_main(opvpn):
+    """ [*]Choose Server Protocol:
+    t    =>    Connect To Tcp Server
+    u    =>    Connect To Udp Server
+    b    =>    Back To Menu         
+"""
+    while True:
+        try:
+            clear_screen()
+            pc=input('\033[1;32;40m'+nord_main.__doc__+'\033[0m\nInput Choice=> ') or None
+            if pc=='b':break
+            elif pc=='t' or pc=='u':nord_location(pc,opvpn)
+        except:pass
+
+def main():
+    opvpn=''
+    hd='\033[1;32;40m [*]Openvpn Executable Path  e.g  C:\\Program Files(x86)\\OpenVPN\\bin\\openvpn.exe\033[0m\n'
+    while True:
+        try:
+            clear_screen()
+            if not opvpn:
+                if name=='nt':opvpn='C:\\Program Files\\OpenVPN\\bin\\openvpn.exe'
+                else:opvpn='openvpn'
+            vp=input('\033[1;32;40m'+__doc__+'\033[0m\nInput Choice=> ') or None
+            if vp=='e':break
+            elif vp=='1':nord_main(opvpn)
+            elif vp=='2':
+                ovp,ove=get_config('alt_configs')
+                vpn_connect(ovp,opvpn)
+            elif vp=='p':
+                clear_screen()
+                opn=input(hd+'\nInput Path Or Pass=> ')
+                if Path(opn).exists():opvpn=opn
+        except:pass
+    clear_screen()
+
+
+if __name__=='__main__':
+    main()
